83a8d858a757edc01e633ff8a485d480d1bf61ae
Justine Fernandez <jfernandez@stratpoint.com>
2019-05-30 10:18:34 +0800

    Revert Changed next to callback

--- app/index.js
+++ app/index.js
@@ -15,12 +15,12 @@ app.use(bodyParser.json())
 app.get('/api/contacts', function (request, response) {
     pool.connect(function (err, client, done) {
         if (err) {
-        return callback(err)
+        return next(err)
         }
         client.query('SELECT * FROM contacts;', [], function (err, result) {
         done()
         if (err) {
-            return callback(err)
+            return next(err)
         }
         response.json(result.rows)
         })
@@ -31,12 +31,12 @@ app.post('/api/contacts', function (request, response) {
     const contacts = request.body
     pool.connect(function (err, client, done) {
       if (err) {
-        return callback(err)
+        return next(err)
       }
       client.query('INSERT INTO contacts (first_name, last_name, address, email_address, contact_number) VALUES ($1, $2, $3, $4, $5);', [contacts.first_name, contacts.last_name, contacts.address, contacts.email_address, contacts.contact_number], function (err, result) {
         done()
         if (err) {
-          return callback(err)
+          return next(err)
         }
         response.send(200)
       })
@@ -47,13 +47,13 @@ app.get('/api/contacts/:id',  function(request, response) {
     const id = request.params.id
     pool.connect(function (err, client, done) {
         if (err) {
-        return callback(err)
+        return next(err)
         }
         var query = "SELECT * FROM contacts WHERE _id = '" + id + "'";
         client.query(query, [], function (err, result) {
         done()
         if (err) {
-            return callback(err)
+            return next(err)
         }
         response.json(result.rows)
         })
@@ -65,13 +65,13 @@ app.put('/api/contacts/:id', function(request, response) {
     const contacts = request.body
     pool.connect(function (err, client, done) {
         if (err) {
-        return callback(err)
+        return next(err)
         }
         var query = "UPDATE contacts SET first_name='"+ contacts.first_name +"', last_name='"+ contacts.last_name +"', address='"+ contacts.address +"', email_address='"+ contacts.email_address +"', contact_number='"+ contacts.contact_number +"' WHERE _id = '" + id + "'";
         client.query(query, [], function (err, result) {
         done()
         if (err) {
-            return callback(err)
+            return next(err)
         }
         response.send(200)
         })
@@ -82,13 +82,13 @@ app.delete('/api/contacts/:id', function(request, response) {
     const id = request.params.id
     pool.connect(function (err, client, done) {
         if (err) {
-        return callback(err)
+        return next(err)
         }
         var query = "DELETE FROM contacts WHERE _id = '" + id + "'";
         client.query(query, [], function (err, result) {
         done()
         if (err) {
-            return callback(err)
+            return next(err)
         }
         response.send(200)
         })
