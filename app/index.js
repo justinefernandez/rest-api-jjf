41a99b6476570b0bef39b50a2fcce44390b1bf5c
Justine Fernandez <jfernandez@stratpoint.com>
2019-05-31 18:41:21 +0800

    Fixed Error on Pool

--- app/index.js
+++ app/index.js
@@ -4,68 +4,62 @@ const { Pool } = require('pg');
 const config = require('../config');
 
 const app = express();
-
 const pool = new Pool(config.redisStore);
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.get('/api/contacts', (request, response) => {
-  pool.query('SELECT * FROM contacts;', (err, res) => {
+app.get('/api/contacts', async (request, response) => {
+  await pool.query('SELECT * FROM contacts;', (err, res) => {
     if (err) {
       response.json(err.stack);
     }
     response.json(res.rows);
   });
-  pool.end();
 });
 
-app.post('/api/contacts', (request, response) => {
+app.post('/api/contacts', async (request, response) => {
   const contacts = request.body;
-  pool.query('INSERT INTO contacts (first_name, last_name, address, email_address, contact_number) VALUES ($1, $2, $3, $4, $5);', [contacts.first_name, contacts.last_name, contacts.address, contacts.email_address, contacts.contact_number], (err, res) => {
+  await pool.query('INSERT INTO contacts (first_name, last_name, address, email_address, contact_number) VALUES ($1, $2, $3, $4, $5);', [contacts.first_name, contacts.last_name, contacts.address, contacts.email_address, contacts.contact_number], (err, res) => {
     if (err) {
       response.json(err.stack);
     }
     response.send(200);
   });
-  pool.end();
 });
 
-app.get('/api/contacts/:id', (request, response) => {
+app.get('/api/contacts/:id', async (request, response) => {
   const { id } = request.params;
   const query = `SELECT * FROM contacts WHERE _id = '${id}'`;
-  pool.query(query, (err, res) => {
+  await pool.query(query, (err, res) => {
     if (err) {
       response.json(err.stack);
     }
     response.json(res.rows);
   });
-  pool.end();
 });
 
-app.put('/api/contacts/:id', (request, response) => {
+app.put('/api/contacts/:id', async (request, response) => {
   const { id } = request.params;
   const contacts = request.body;
   const query = `UPDATE contacts SET first_name='${contacts.first_name}', last_name='${contacts.last_name}', address='${contacts.address}', email_address='${contacts.email_address}', contact_number='${contacts.contact_number}' WHERE _id = '${id}'`;
-  pool.query(query, (err, res) => {
+  await pool.query(query, (err, res) => {
     if (err) {
       response.json(err.stack);
     }
     response.json(res.rows);
   });
-  pool.end();
 });
 
-app.delete('/api/contacts/:id', (request, response) => {
+app.delete('/api/contacts/:id', async (request, response) => {
   const { id } = request.params;
   const query = `DELETE FROM contacts WHERE _id = '${id}'`;
-  pool.query(query, (err, res) => {
+  await pool.query(query, (err, res) => {
     if (err) {
       response.json(err.stack);
     }
     response.json(res.rows);
   });
-  pool.end();
 });
 
 module.exports = app;
