d45fd8b16ae43a883f9e6407a51026a5c7f3e215
Justine Fernandez <jfernandez@stratpoint.com>
2019-05-31 18:18:09 +0800

    Moved: Pool.end on the top

--- app/index.js
+++ app/index.js
@@ -12,6 +12,7 @@ app.use(bodyParser.json());
 
 app.get('/api/contacts', (request, response) => {
   pool.query('SELECT * FROM contacts;', (err, res) => {
+    pool.end();
     if (err) {
       response.json(err.stack);
     }
@@ -23,10 +24,10 @@ app.get('/api/contacts', (request, response) => {
 app.post('/api/contacts', (request, response) => {
   const contacts = request.body;
   pool.query('INSERT INTO contacts (first_name, last_name, address, email_address, contact_number) VALUES ($1, $2, $3, $4, $5);', [contacts.first_name, contacts.last_name, contacts.address, contacts.email_address, contacts.contact_number], (err, res) => {
+    pool.end();
     if (err) {
       response.json(err.stack);
     }
-    pool.end();
     response.send(200);
   });
 });
@@ -35,6 +36,7 @@ app.get('/api/contacts/:id', (request, response) => {
   const { id } = request.params;
   const query = `SELECT * FROM contacts WHERE _id = '${id}'`;
   pool.query(query, (err, res) => {
+    pool.end();
     if (err) {
       response.json(err.stack);
     }
@@ -48,10 +50,10 @@ app.put('/api/contacts/:id', (request, response) => {
   const contacts = request.body;
   const query = `UPDATE contacts SET first_name='${contacts.first_name}', last_name='${contacts.last_name}', address='${contacts.address}', email_address='${contacts.email_address}', contact_number='${contacts.contact_number}' WHERE _id = '${id}'`;
   pool.query(query, (err, res) => {
+    pool.end();
     if (err) {
       response.json(err.stack);
     }
-    pool.end();
     response.json(res.rows);
   });
 });
@@ -60,10 +62,10 @@ app.delete('/api/contacts/:id', (request, response) => {
   const { id } = request.params;
   const query = `DELETE FROM contacts WHERE _id = '${id}'`;
   pool.query(query, (err, res) => {
+    pool.end();
     if (err) {
       response.json(err.stack);
     }
-    pool.end();
     response.json(res.rows);
   });
 });
